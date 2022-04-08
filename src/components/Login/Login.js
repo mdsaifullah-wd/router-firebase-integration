449b95908b8f99a18f12ab7d40ca030634b0cf76
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-04-09 01:56:36 +0600

    redirect user to current page after login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { auth } from '../../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || '/';
+
+  const handleSignInWithGoogle = () => {
+    signInWithGoogle().then(() => {
+      navigate(from, { replace: true });
+    });
+  };
   return (
     <div>
       <h2>Please login</h2>
-      <button onClick={() => signInWithGoogle()} style={{ marginTop: '20px' }}>
+      <button onClick={handleSignInWithGoogle} style={{ marginTop: '20px' }}>
         Sign in With Google
       </button>
       <form>
